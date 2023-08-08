ac49ef78783c5eecef4b87b708df876daacd1278
Mu Yaser <mu.yaser@hotmail.com>
2023-08-08 21:45:00 +0300

    added css modules

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppLayout from "./AppLayout";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
-      <h1>WorldWise</h1>
+      <AppLayout />
+      <h1 className="test">WorldWise</h1>
+
+      <Link to="/app">go to app</Link>
     </div>
   );
 }
