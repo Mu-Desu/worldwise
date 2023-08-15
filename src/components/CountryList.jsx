1538969e3159d502aec262378f0d0feda33c4807
unknown <mu.yaser@hotmail.com>
2023-08-15 01:04:07 -0700

    refactored the app to use context instead of having the logic inside the app

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,9 +2,12 @@ import CountryItem from "./CountryItem";
 import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
 //eslint-disable-next-line react/prop-types
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   //eslint-disable-next-line react/prop-types
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,35 +1,47 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const BASE_URL = "http://localhost:9000";
 
-const CitiesContext = createContext()
+const CitiesContext = createContext();
 
 //eslint-disable-next-line react/prop-types
-function CitiesProvider({children}){
-    const [cities, setCities] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-  
-    useEffect(function () {
-      async function fetchCities() {
-        try {
-          setIsLoading(true);
-          const res = await fetch(`${BASE_URL}/cities`);
-          const data = await res.json();
-          setCities(data);
-        } catch {
-          alert("There was an error loading data...");
-        } finally {
-          setIsLoading(false);
-        }
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
       }
-      fetchCities();
-    }, []);
+    }
+    fetchCities();
+  }, []);
 
-    return <CitiesContext.Provider value={{
-        cities,isLoading
-    }}>
-        {children}
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
     </CitiesContext.Provider>
-}   
+  );
+}
 
-export {CitiesProvider}
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside of CitiesProvider");
+  return context;
+}
+
+export { CitiesProvider, useCities };
